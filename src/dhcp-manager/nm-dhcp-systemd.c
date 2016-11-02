9dbee76b04f2e214621e2c3fc0df09eab48daf80
Simon Fels <simon.fels@canonical.com>
2016-11-02 11:49:26 +0100

    Revert "[SNAPPY] Write DHCP lease files to /run/NetworkManager/dhcp/"
    This reverts commit 2331dd5f46178b6bb6d0ea3bf8cfb953feeb74d3.

--- src/dhcp-manager/nm-dhcp-systemd.c
+++ src/dhcp-manager/nm-dhcp-systemd.c
@@ -398,7 +398,8 @@ lease_to_ip4_config (const char *iface,
 static char *
 get_leasefile_path (const char *iface, const char *uuid, gboolean ipv6)
 {
-	return g_strdup_printf ("/run/NetworkManager/dhcp/internal%s-%s-%s.lease",
+	return g_strdup_printf ("%s/internal%s-%s-%s.lease",
+							nm_utils_get_state_dir(),
 	                        ipv6 ? "6" : "",
 	                        uuid,
 	                        iface);
