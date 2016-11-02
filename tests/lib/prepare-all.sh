1cf623c16e7ac252ee1e46f5aa563e456714ab95
Simon Fels <simon.fels@canonical.com>
2016-11-02 15:36:14 +0100

    Rework test setup so we have two different suites

---
+++ tests/lib/prepare-all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Setup classic snap and build the network-manager snap in there
+snap install --devmode --beta classic
+cat <<-EOF > /home/test/build-snap.sh
+#!/bin/sh
+set -ex
+apt update
+apt install -y --force-yes snapcraft
+cd /home/network-manager
+snapcraft clean
+snapcraft
+EOF
+chmod +x /home/test/build-snap.sh
+sudo classic /home/test/build-snap.sh
+snap remove classic
+
+# Make sure we have a snap build
+test -e /home/network-manager/network-manager_*_amd64.snap
--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-if [ $SPREAD_REBOOT -ge 1 ] ; then
-	# Give system a moment to settle
-	sleep 2
-
-	# For debugging dump all snaps and connected slots/plugs
-	snap list
-	snap interfaces
-
-	exit 0
-fi
-
 echo "Wait for firstboot change to be ready"
 while ! snap changes | grep -q "Done"; do
 	snap changes || true
@@ -37,16 +26,7 @@ snap list | grep ^${kernel_name} | grep -E " [0-9]+\s+canonical"
 if [ -n "$SNAP_CHANNEL" ] ; then
 	snap install --$SNAP_CHANNEL network-manager
 else
-	# Setup classic snap and build the network-manager snap in there
-	snap install --devmode --beta classic
-	cat <<-EOF > /home/test/build-snap.sh
-	#!/bin/sh
-	set -ex
-	apt update
-	apt install -y --force-yes snapcraft
-	cd /home/network-manager
-	snapcraft clean
-	snapcraft
+	# Install prebuilt network-manager snap
 	snap install --dangerous network-manager_*_amd64.snap
 	# As we have a snap which we build locally its unasserted and therefor
 	# we don't have any snap-declarations in place and need to manually
@@ -54,9 +34,6 @@ else
 	snap connect network-manager:nmcli network-manager:service
 	snap connect network-manager:network-setup-observe
 	snap connect network-manager:ppp
-	EOF
-	chmod +x /home/test/build-snap.sh
-	sudo classic /home/test/build-snap.sh
 fi
 
 # Snapshot of the current snapd state for a later restore
@@ -66,6 +43,6 @@ if [ ! -f $SPREAD_PATH/snapd-state.tar.gz ] ; then
 	systemctl start snapd.socket
 fi
 
-# We need to reboot the system in order to get NetworkManager
-# configured as the main network management service.
-REBOOT
+# For debugging dump all snaps and connected slots/plugs
+snap list
+snap interfaces
