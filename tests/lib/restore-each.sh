b51386d38482ddc461f0456c9693f21073ebfda9
Simon Fels <simon.fels@canonical.com>
2016-12-13 12:05:34 +0100

    tests: move snap install into own function and install/remove correctly

--- tests/lib/restore-each.sh
+++ tests/lib/restore-each.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 . $TESTSLIB/snap-names.sh
+. $TESTSLIB/utilities.sh
 
 # Remove all snaps not being the core, gadget, kernel or snap we're testing
 for snap in /snap/*; do
@@ -14,12 +15,6 @@ for snap in /snap/*; do
 	esac
 done
 
-# Cleanup all configuration files from NetworkManager so that we have
-# a fresh start for the next test
-rm -rf /var/snap/network-manager/common/*
-rm -rf /var/snap/network-manager/current/*
-systemctl stop snap.network-manager.networkmanager
-
 # Drop any generated or modified netplan configuration files. The original
 # ones will be restored below.
 rm -f /etc/netplan/*
@@ -27,13 +22,15 @@ rm -f /etc/netplan/*
 # Ensure we have the same state for snapd as we had before
 systemctl stop snapd.service snapd.socket
 rm -rf /var/lib/snapd/*
-$(cd / && tar xzf $SPREAD_PATH/snapd-state.tar.gz)
+tar xzf $SPREAD_PATH/snapd-state.tar.gz -C /
 rm -rf /root/.snap
 systemctl start snapd.service snapd.socket
+
+systemctl stop snap.network-manager.networkmanager
+rm -rf /var/snap/network-manager/*
+tar xzf $SPREAD_PATH/nm-state.tar.gz -C /
+systemctl start snap.network-manager.networkmanager
 
 # Make sure the original netplan configuration is applied and active
 netplan generate
 netplan apply
-
-# Bringup NetworkManager again now that the system is restored
-systemctl start snap.network-manager.networkmanager
