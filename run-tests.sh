537dda152a737eccafd0f839a2690818a128a7fb
Konrad Zapałowicz <konrad.zapalowicz@canonical.com>
2017-01-24 12:14:27 +0100

    tests: fetch the remote repository and execute tests runner
    The run-tests.sh script now delegates the tests execution to the test runner
    which is in the tests-extras repository passing arguments there as-is.

--- run-tests.sh
+++ run-tests.sh
@@ -16,97 +16,58 @@
 
 set -e
 
-image_name=ubuntu-core-16.img
-channel=stable
-snap=
-spread_opts=
-force_new_image=0
-test_from_channel=0
+# ==============================================================================
+# This has to be in sync with the tests-extras/run-tests.sh script
+# functionalities. 
 
 show_help() {
     echo "Usage: run-tests.sh [OPTIONS]"
     echo
-    echo "Uses spread to execute tests. If required or prompted can generate"
-    echo "Ubuntu Core image for testing."
+    echo "This is fetch & forget script and what it does is to fetch the"
+    echo "tests-extras repository and execute the run-tests.sh script from"
+    echo "there passing arguments as-is."
     echo
     echo "optional arguments:"
     echo "  --help                 Show this help message and exit"
-    echo "  --channel              Select another channel to build the base image from (default: $channel)"
-    echo "  --snap		   Extra snap to install"
+    echo "  --channel              Select another channel to build the base image from (default: stable)"
+    echo "  --snap		 Extra snap to install"
     echo "  --debug                Enable verbose debugging output"
     echo "  --test-from-channel    Pull network-manager snap from the specified channel instead of building it from source"
     echo "  --force-new-image      Force generating a new image used for testing"
 }
 
-while [ -n "$1" ]; do
-	case "$1" in
-		--help)
-			show_help
-			exit
-			;;
-		--channel=*)
-			channel=${1#*=}
-			shift
-			;;
-		--snap=*)
-			snap=${1#*=}
-			shift
-			;;
-		--test-from-channel)
-			test_from_channel=1
-			shift
-			;;
-		--debug)
-			spread_opts="$spread_opts -vv -debug"
-			shift
-			;;
-		--force-new-image)
-			force_new_image=1
-			shift
-			;;
-		*)
-			echo "Unknown command: $1"
-			exit 1
-			;;
-	esac
-done
+# ==============================================================================
+# This is fetch & forget script and what it does is to fetch the tests-extras
+# repo and execute the run-tests.sh script from there passing arguments as-is.
 
-SPREAD_QEMU_PATH="$HOME/.spread/qemu"
-if [ `which spread` = /snap/bin/spread ] ; then
-	current_version=`readlink /snap/spread/current`
-	SPREAD_QEMU_PATH="$HOME/snap/spread/$current_version/.spread/qemu/"
+# 0. Display help w/o fetching anything
+
+if [ "$1" == "--help" ]; then
+	show_help
+	exit 0
 fi
 
-# Make sure we have a base image we use for testing
-if [ ! -e $SPREAD_QEMU_PATH/$image_name ] || [ $force_new_image -eq 1 ] ; then
+# 1. Fetch
 
-	# Get the image creating scripts
-	CREATE_IMAGE_URL="https://git.launchpad.net/~snappy-hwe-team/snappy-hwe-snaps/+git/create-image-scripts"
-	TESTS_EXTRAS_PATH="tests-extras"
-	CREATE_IMAGE_PATH=$TESTS_EXTRAS_PATH"/create-image-scripts"
+TESTS_EXTRAS_URL="https://git.launchpad.net/~snappy-hwe-team/snappy-hwe-snaps/+git/tests-extras"
+TESTS_EXTRAS_PATH="tests-extras"
 
-	if [ -d "$TESTS_EXTRAS_PATH" ]; then
-		rm -rf $TESTS_EXTRAS_PATH
-	fi
-	echo "INFO: Fetching test image creating scripts into $TESTS_EXTRAS_PATH ..."
-	(git clone -b master $CREATE_IMAGE_URL $TESTS_EXTRAS_PATH >/dev/null 2>&1)
-	if [ $? -ne 0 ]; then
-		echo "ERROR: Failed to fetch the $CREATE_IMAGE_URL repo, exiting.."
-		exit 1
-	fi
+# delete the fetched content on ctrl-c
+trap 'cd .. && if [ -d "$TESTS_EXTRAS_PATH" ]; then rm -rf $TESTS_EXTRAS_PATH; fi' INT TERM EXIT
 
-	echo "INFO: Creating new qemu test image ..."
-	(cd $CREATE_IMAGE_PATH; sudo ./create-image.sh $channel $snap)
-	mkdir -p $SPREAD_QEMU_PATH
-	mv $CREATE_IMAGE_PATH/ubuntu-core-16.img $SPREAD_QEMU_PATH/$image_name
-
-	# Not needed at this point
+if [ -d "$TESTS_EXTRAS_PATH" ]; then
 	rm -rf $TESTS_EXTRAS_PATH
 fi
 
-# We currently only run spread tests but we could do other things
-# here as well like running our snap-lintian tool etc.
-if [ $test_from_channel -eq 1 ] ; then
-	export SNAP_CHANNEL=$channel
+echo "INFO: Fetching tests-extras scripts into $TESTS_EXTRAS_PATH ..."
+(git clone -b master $TESTS_EXTRAS_URL $TESTS_EXTRAS_PATH >/dev/null 2>&1)
+if [ $? -ne 0 ]; then
+	echo "ERROR: Failed to fetch the $TESTS_EXTRAS_URL repo, exiting.."
+	exit 1
 fi
-spread $spread_opts
+
+# 2. Execute
+cd $TESTS_EXTRAS_PATH && ./tests-runner.sh $@
+
+# 3. Done; clean-up
+cd .. && if [ -d "$TESTS_EXTRAS_PATH" ]; then rm -rf $TESTS_EXTRAS_PATH; fi
