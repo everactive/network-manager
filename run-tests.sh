b59d07627c64f0b2769444ae35f79065ada8640c
Konrad Zapałowicz <konrad.zapalowicz@canonical.com>
2017-01-25 16:20:16 +0100

    add .gitignore to the repository
    This commit adds the .gitignore file and thus makes the part of
    script where this is done unnecessary.

--- run-tests.sh
+++ run-tests.sh
@@ -18,7 +18,6 @@ set -e
 
 TESTS_EXTRAS_URL="https://git.launchpad.net/~snappy-hwe-team/snappy-hwe-snaps/+git/tests-extras"
 TESTS_EXTRAS_PATH=".tests-extras"
-GITIGNORE=".gitignore"
 
 # Display help.
 # This has to be in sync with the tests-extras/test-runner.sh script
@@ -49,47 +48,6 @@ clone_tests_extras() {
 	fi
 }
 
-# Commit the .gitignore file
-commit_gitignore() {
-	echo "INFO: Commiting $GITIGNORE"
-	(git add $GITIGNORE && git commit -m "$TESTS_EXTRAS_PATH should not be tracked" >/dev/null 2>&1)
-	if [ $? -ne 0 ]; then
-		echo "WARN: Failed to add $TESTS_EXTRAS_PATH to .gitignore and commit, script will continue but please check"
-	fi
-}
-
-# Tell git to ignore the tests-extras subdirectory
-# It updates the .gitignore file and commits it unless it has local
-# modifications. The .gitignore is:
-# - created and commited if does not yet exists.
-# - updated and commited if exists and w/o local modifications
-# - updated and not commited if exists and with local modifications
-add_tests_extras_to_gitignore() {
-	echo "INFO: Adding tests-extras to gitignore"
-	if [ ! -f "$GITIGNORE" ]; then
-		echo $TESTS_EXTRAS_PATH > $GITIGNORE
-		commit_gitignore
-	else
-		(git status -s $GITIGNORE | grep -q M)
-		should_commit=$?
-
-		(grep $TESTS_EXTRAS_PATH -q $GITIGNORE)
-		not_yet_ignored=$?
-		if [ $not_yet_ignored -eq 1 ]; then
-
-			echo $TESTS_EXTRAS_PATH >> $GITIGNORE
-			
-			if [ $should_commit -eq 1 ]; then
-				commit_gitignore
-			else
-				echo "WARN: Your .gitignore has been updated but cannot be commited, script will continue but please check"
-			fi
-		else
-			echo "INFO: $TESTS_EXTRAS_PATH already ignored by git"
-		fi
-	fi
-}
-
 # Make sure the already cloned tests-extras repository is in a known and update
 # state before it is going to be used.
 restore_and_update_tests_extras() {
@@ -103,8 +61,7 @@ restore_and_update_tests_extras() {
 # repo and execute the run-tests.sh script from there passing arguments as-is.
 #
 # The tests-extras repository ends up checked out in the snap tree but as a
-# hidden directory which is re-used since then. Also it is added to .gitignore
-# so that it does not end-up being tracked by accident. 
+# hidden directory which is re-used since then.
 
 # Display help w/o fetching anything
 if [ "$1" == "--help" ]; then
@@ -117,7 +74,6 @@ if [ -d "$TESTS_EXTRAS_PATH" ]; then
 else
 	clone_tests_extras
 fi
-add_tests_extras_to_gitignore
 
 echo "INFO: Executing tests runner"
 cd $TESTS_EXTRAS_PATH && ./tests-runner.sh $@
