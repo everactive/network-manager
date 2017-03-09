6c455ed315cec7a20fa9630f0dc7615eac4670a5
Simon Fels <simon.fels@canonical.com>
2017-03-09 08:19:48 +0100

    Sync run-tests.sh script with the latest template from tests-extras repository

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Copyright (C) 2016 Canonical Ltd
 #
@@ -19,29 +19,24 @@ set -e
 TESTS_EXTRAS_URL="https://git.launchpad.net/~snappy-hwe-team/snappy-hwe-snaps/+git/tests-extras"
 TESTS_EXTRAS_PATH=".tests-extras"
 
-# Display help.
-# This has to be in sync with the tests-extras/test-runner.sh script
-# functionalities as the parameters to this one are passed directly there
 show_help() {
-    echo "Usage: run-tests.sh [OPTIONS]"
-    echo
-    echo "This is fetch & forget script and what it does is to fetch the"
-    echo "tests-extras repository and execute the run-tests.sh script from"
-    echo "there passing arguments as-is."
-    echo
-    echo "optional arguments:"
-    echo "  --help                 Show this help message and exit"
-    echo "  --channel              Select another channel to build the base image from (default: stable)"
-    echo "  --snap                 Extra snap to install"
-    echo "  --debug                Enable verbose debugging output"
-    echo "  --test-from-channel    Pull network-manager snap from the specified channel instead of building it from source"
-    echo "  --force-new-image      Force generating a new image used for testing"
+    exec cat <<'EOF'
+Usage: run-tests.sh [OPTIONS]
+
+This is fetch & forget script and what it does is to fetch the
+tests-extras repository and execute the run-tests.sh script from
+there passing arguments as-is.
+
+When you see this message you don't have the tests-extras repository
+successfully populated in your workspace yet. Please rerun without
+specifying --help to proceed with the initial clone of the git repository.
+EOF
 }
 
 # Clone the tests-extras repository
 clone_tests_extras() {
 	echo "INFO: Fetching tests-extras scripts into $TESTS_EXTRAS_PATH ..."
-	(git clone -b master $TESTS_EXTRAS_URL $TESTS_EXTRAS_PATH >/dev/null 2>&1)
+	git clone -b master $TESTS_EXTRAS_URL $TESTS_EXTRAS_PATH >/dev/null 2>&1
 	if [ $? -ne 0 ]; then
 		echo "ERROR: Failed to fetch the $TESTS_EXTRAS_URL repo, exiting.."
 		exit 1
@@ -52,8 +47,8 @@ clone_tests_extras() {
 # state before it is going to be used.
 restore_and_update_tests_extras() {
 	echo "INFO: Restoring and updating $TESTS_EXTRAS_PATH"
-	(cd $TESTS_EXTRAS_PATH && git reset --hard && git clean -dfx && git pull)
-	(cd ..)
+	cd $TESTS_EXTRAS_PATH && git reset --hard && git clean -dfx && git pull
+	cd -
 }
 
 # ==============================================================================
@@ -63,11 +58,7 @@ restore_and_update_tests_extras() {
 # The tests-extras repository ends up checked out in the snap tree but as a
 # hidden directory which is re-used since then.
 
-# Display help w/o fetching anything
-if [ "$1" == "--help" ]; then
-	show_help
-	exit 0
-fi
+[ ! -d "$TESTS_EXTRAS_PATH" ] && [ "$1" = "--help" ] && show_help
 
 if [ -d "$TESTS_EXTRAS_PATH" ]; then
 	restore_and_update_tests_extras
@@ -76,4 +67,4 @@ else
 fi
 
 echo "INFO: Executing tests runner"
-cd $TESTS_EXTRAS_PATH && ./tests-runner.sh $@
+cd $TESTS_EXTRAS_PATH && ./tests-runner.sh "$@"
