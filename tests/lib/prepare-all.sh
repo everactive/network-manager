f08dee9be445992f9da6977f8c0121723547069f
Simon Fels <simon.fels@canonical.com>
2017-03-16 13:51:11 +0100

    Add comment to explain why we load the mac8022_hwsim driver on system boot

--- tests/lib/prepare-all.sh
+++ tests/lib/prepare-all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Create a service to get the mac80211_hwsim driver loaded on system
+# startup so that we don't need to load it again in our tests as
+# load/unload multiple times in a running system can lead to kernel
+# crashes.
 cat << EOF > /etc/systemd/system/load-mac80211-hwsim.service
 [Unit]
 Description=Load mac8022_hwsim driver
