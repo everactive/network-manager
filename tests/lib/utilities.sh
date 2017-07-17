7e1e09803095d8b55b9df32a7ecc49f099a6262d
Roberto Mier Escandon <rmescandon@gmail.com>
2017-07-17 13:35:19 +0200

    sleep enough time after systemd service is up to prevent retart repeated too quickly error

--- tests/lib/utilities.sh
+++ tests/lib/utilities.sh
@@ -34,11 +34,40 @@ switch_netplan_to_networkd() {
 	rm /etc/netplan/00-default-nm-renderer.yaml
 }
 
+# waits for a service to be active. Besides that, waits enough
+# time after detecting it to be active to prevent restarting
+# same service too quickly several times.
+# $1 service name
+# $2 start limit interval in seconds (default set to 10. Exec `systemctl show THE_SERVICE -p StartLimitInterval` to verify)
+# $3 start limit burst. Times allowed to start the service in start_limit_interval time (default set to 5. Exec `systemctl show THE_SERVICE -p StartLimitBurst` to verify)
 wait_for_systemd_service() {
 	while ! systemctl status $1 ; do
 		sleep 1
 	done
-	sleep 1
+  # As debian services default limit is to allow 5 restarts in a 10sec period
+  # (StartLimitInterval=10000000 and StartLimitBurst=5), having enough wait time we
+  # prevent "service: Start request repeated too quickly" error.
+  #
+  # You can check those values for certain service by typing:
+  #   $systemctl show THE_SERVICE -p StartLimitInterval,StartLimitBurst
+  #
+  if [ $# -ge 2 ]; then
+    start_limit_interval = $2
+  else
+    start_limit_interval=$(systemctl show $1 -p StartLimitInterval | sed 's/StartLimitInterval=//')
+    # original limit interval is provided in microseconds.
+    start_limit_interval=$((start_limit_interval / 1000000))
+  fi
+
+  if [ $# -eq 3 ]; then
+    start_limit_burst = $3
+  else
+    start_limit_burst=$(systemctl show $1 -p StartLimitBurst | sed 's/StartLimitBurst=//')
+  fi
+
+  # adding 1 to be sure we exceed the limit
+  sleep_time=$((1 + $start_limit_interval / $start_limit_burst))
+	sleep $sleep_time
 }
 
 wait_for_network_manager() {
